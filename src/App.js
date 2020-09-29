b32c0d928398cbe4baccaefe222231239236781f
Helena <helena.oursel@gmail.com>
2020-09-29 09:31:03 +0200

    Test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import TodoList  from './Todo/TodoList';
+
+function App() {
+  return (
+    <TodoList />
+  );
+}
+
+export default App;
